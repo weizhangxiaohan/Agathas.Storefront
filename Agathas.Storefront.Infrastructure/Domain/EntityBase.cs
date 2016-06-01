a76499dc39dfb08094bcd50fb94bfd50327ee5eb
weizhangxiaohan_C <marvin.wei@praemium.com>
2016-06-01 18:07:03 +0800

    go

---
+++ Agathas.Storefront.Infrastructure/Domain/EntityBase.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agathas.Storefront.Infrastructure.Domain
+{
+    public abstract class EntityBase<TId>
+    {
+        private List<BusinessRule> _brokenRules = new List<BusinessRule>();
+
+        public TId Id { get; set; }
+
+        protected abstract void Validate();
+
+        public IEnumerable<BusinessRule> GetBrokenRules() 
+        {
+            _brokenRules.Clear();
+            Validate();
+            return _brokenRules;
+        }
+
+        protected void AddBrokenRule(BusinessRule businessRule) 
+        {
+            _brokenRules.Add(businessRule);
+        }
+
+        public override bool Equals(object entity)
+        {
+            return entity != null
+                && entity is EntityBase<TId>
+                && this == (EntityBase<TId>)entity;
+        }
+
+        public override int GetHashCode()
+        {
+            return this.Id.GetHashCode();
+        }
+    }
+}
