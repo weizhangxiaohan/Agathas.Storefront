f6a1cb814b1e7fd7de9cfdd8c10bec3bbf91b91e
WeiZhangXiaoHan_Home <191689072@qq.com>
2016-06-02 22:47:03 +0800

    go

---
+++ Agathas.Storefront.Infrastructure/Configuration/WebConfigApplicationSettings.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agathas.Storefront.Infrastructure.Configuration
+{
+    public class WebConfigApplicationSettings : IApplicationSettings
+    {
+        public string LoggerName
+        {
+            get { return ConfigurationManager.AppSettings["LoggerName"]; }
+        }
+    }
+}
---
+++ Agathas.Storefront.Infrastructure/Querying/CriteriaOperator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agathas.Storefront.Infrastructure.Querying
+{
+    public enum CriteriaOperator
+    {
+        Equal,
+        LesserThanOrEqual,
+        NotApplicable
+        // TODO: Add the remainder of the criteria operators as required.
+    }
+}
